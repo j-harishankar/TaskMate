01ff3b9f6b5d9e5517a23e5175b4fea7e356433e
j harishankar <jharivichu27@gmail.com>
2025-06-19 23:09:12 +0530

    model form added

--- todoproject/todo/urls.py
+++ todoproject/todo/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns =[
-    path('',views.task_list,name='task-list'),
+    path('',views.task_list,name='list'),
+    path('create/',views.create_task,name='create')
 ]
--- todoproject/todo/views.py
+++ todoproject/todo/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Task
+from .forms import TaskForm
 # Create your views here.
 def task_list(request):
     tasks = Task.objects.all()
     return render(request,'todo/task_list.html',{'tasks':tasks})
+
+
+def create_task(request):
+    if request.method == 'POST':#
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('list')
+    else:
+        form = TaskForm()
+    return render(request, 'todo/create.html', {'form': form})
